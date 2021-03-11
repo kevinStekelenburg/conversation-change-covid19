ead9ed77a823394acd21d4dc9693f74c5632d3ce
stanwiggers <63144448+stanwiggers@users.noreply.github.com>
2021-03-11 13:29:02 +0100

    Update download_data.R

--- src/data-preparation/download_data.R
+++ src/data-preparation/download_data.R
@@ -1,5 +1,5 @@
 # Download dataset Eredivisie
 # dir.create('./data/dataset1')  # Uncomment if need to create directory with R
-download.file('https://github.com/stanwiggers/tweets_eredivisie_data/blob/main/merged_tweets_eredivisie.csv','././data/dataset_eredivisie.csv')
+download.file('https://github.com/stanwiggers/tweets_eredivisie_data/blob/main/merged_tweets_eredivisie.csv','././data/dataset1/dataset_eredivisie.csv')
 
 

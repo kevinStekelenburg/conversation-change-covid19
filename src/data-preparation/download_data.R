566080e441eac77d9d2f3a1f16c3c6ee08da3a7f
Kevin Stekelenburg <kevinstekelenburg@MBP-van-Kevin.home>
2021-03-18 21:24:48 +0100

    Add print statement to display in terminal

--- src/data-preparation/download_data.R
+++ src/data-preparation/download_data.R
@@ -1,5 +1,5 @@
 # Download dataset Eredivisie
-# dir.create('./data/dataset1')  # Uncomment if need to create directory with R
+print("Downloading database...")
 download.file('https://www.dropbox.com/s/yklyw87gkflyfwe/merged_tweets_eredivisie.csv?dl=1','././data/dataset_eredivisie.csv')
 
 

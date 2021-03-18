310f866e994920f44309bc1c97df55d1131788f8
Kevin Stekelenburg <kevinstekelenburg@MBP-van-Kevin.home>
2021-03-18 21:31:04 +0100

    update_input.r for analysis phase

--- src/analysis/update_input.R
+++ src/analysis/update_input.R
@@ -1,2 +1,4 @@
 # Copy output from data-preparation into input folder 
-file.copy("./gen/data-preparation/output/merged_sentiment.RData","./gen/analysis/input/merged_sentiment.RData")
+file.copy(here("gen", "data-preparation", "output", "tdm_post.csv"), here("gen", "analysis", "input", "tdm_post.csv"))
+file.copy(here("gen", "data-preparation", "output", "tdm_pre.csv"), here("gen", "analysis", "input", "tdm_pre.csv"))
+file.copy(here("gen", "data-preparation", "output", "merged_sentiment.csv"), here("gen", "analysis", "input", "merged_sentiment.csv"))

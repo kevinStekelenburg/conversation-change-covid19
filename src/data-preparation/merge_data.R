fa5512a28950936496583045949edb6a98e4ad75
stan wiggers <s.b.wiggers@tilburguniversity.edu>
2021-03-18 15:10:04 +0100

    clean_data.R changes

--- src/data-preparation/merge_data.R
+++ src/data-preparation/merge_data.R
@@ -3,7 +3,7 @@
 print("Reading pre- and post-corona dataframes for sentiment analysis...")
 
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-load(file=here("gen", "data-preparation", "temp", "scores_pre.Rdata"))
-load(file=here("gen", "data-preparation", "temp", "scores_post.Rdata"))
+load(file=here("gen", "data-preparation", "output", "scores_pre.Rdata"))
+load(file=here("gen", "data-preparation", "output", "scores_post.Rdata"))
 
 

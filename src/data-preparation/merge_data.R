7c02ec8ff022e0867c9af4a569adad0dfb3be189
Kevin Stekelenburg <kevinstekelenburg@MBP-van-Kevin.home>
2021-03-18 14:20:10 +0100

    Created mergefile to merge sentiment analysis dataframes

---
+++ src/data-preparation/merge_data.R
@@ -0,0 +1,9 @@
+# this file is used for merging the output dataframes "pre" and "post" which scores each tweet on different criteria
+# INPUT ----
+print("Reading pre- and post-corona dataframes for sentiment analysis...")
+
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+load(file=here("gen", "data-preparation", "temp", "scores_pre.Rdata"))
+load(file=here("gen", "data-preparation", "temp", "scores_post.Rdata"))
+
+

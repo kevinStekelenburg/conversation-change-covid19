a59a0ddb852d1f92eba577c12f11a079b921eb13
Kevin Stekelenburg <kevinstekelenburg@MBP-van-Kevin.home>
2021-03-21 16:42:44 +0100

    Script added to download.R to create full gen dir to store generated files

--- src/data-preparation/download_data.R
+++ src/data-preparation/download_data.R
@@ -2,4 +2,27 @@
 print("Downloading database...")
 download.file('https://www.dropbox.com/s/yklyw87gkflyfwe/merged_tweets_eredivisie.csv?dl=1','././data/dataset_eredivisie.csv')
 
+# Loading great package for dealing with relative path errors
+if(!require(here)){
+  install.packages('here',repos = "http://cran.us.r-project.org")
+  library(here)
+}  
+
+# Create GEN directory
+print("Creating 'gen' directory...")
+dir.create(here("gen"))
+dir.create(here("gen", "analysis"))
+dir.create(here("gen", "data-preparation"))
+dir.create(here("gen", "paper"))
+dir.create(here("gen", "analysis", "input"))
+dir.create(here("gen", "analysis", "output"))
+dir.create(here("gen", "analysis", "temp"))
+dir.create(here("gen", "data-preparation", "input"))
+dir.create(here("gen", "data-preparation", "output"))
+dir.create(here("gen", "data-preparation", "temp"))
+dir.create(here("gen", "paper", "input"))
+dir.create(here("gen", "paper", "output"))
+dir.create(here("gen", "paper", "temp"))
+
+
 
